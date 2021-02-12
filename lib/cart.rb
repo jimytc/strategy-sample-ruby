dcb231f8e87a06d5a4f820a6ac98cc64f2c891d0
jimytc <solofat@gmail.com>
2021-02-12 21:33:57 +0800

    refactor: extract method fee_by_hsinchu

--- lib/cart.rb
+++ lib/cart.rb
@@ -7,11 +7,7 @@ class Cart
     if shipper == :black_cat
       fee_by_black_cat(product)
     elsif shipper == :hsinchu
-      if product.length > 100 or product.width > 100 or product.height > 100
-        product.size * 0.00002 * 1100 + 500
-      else
-        product.size * 0.00002 * 1200
-      end
+      fee_by_hsinchu(product)
     elsif shipper == :post_office
       fee_by_weight = 80 + product.weight * 10
       fee_by_size = product.size * 0.00002 * 1100
@@ -22,6 +18,14 @@ class Cart
   end
 
   private
+
+  def fee_by_hsinchu(product)
+    if product.length > 100 or product.width > 100 or product.height > 100
+      product.size * 0.00002 * 1100 + 500
+    else
+      product.size * 0.00002 * 1200
+    end
+  end
 
   def fee_by_black_cat(product)
     if product.weight > 20
